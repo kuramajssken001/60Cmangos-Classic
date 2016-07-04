10b9f1fe0f2d15cb22cacda91275f487435e2cc1
kuramajssken001 <kuramajssken1234567@gmail.com>
2016-07-04 13:01:32 +0800

    Fix dupe/crash V2

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -2555,6 +2555,7 @@ void Spell::prepare(SpellCastTargets const* targets, Aura* triggeredByAura)
 
     // add non-triggered (with cast time and without)
     else if (!m_IsTriggeredSpell)
+	if (!m_IsTriggeredSpell)
     {
         // add to cast type slot
         m_caster->SetCurrentCastedSpell(this);
@@ -2563,9 +2564,14 @@ void Spell::prepare(SpellCastTargets const* targets, Aura* triggeredByAura)
         SendSpellStart();
 
         TriggerGlobalCooldown();
+		// Execute instant spells immediate
 		if (m_timer == 0 && !IsNextMeleeSwingSpell() && !IsAutoRepeat() && !IsChanneledSpell(m_spellInfo))
-			cast();
-    }
+			 cast();
+		
+	}
+	    // execute triggered without cast time explicitly in call point
+		else if (m_timer == 0)
+			cast(true);
     
     // else triggered with cast time will execute execute at next tick or later
     // without adding to cast type slot
